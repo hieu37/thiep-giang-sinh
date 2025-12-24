ef1a3b5821c6f4cc2bfcb78e3e460ddd1f54892f
hieu37 <67743457+hieu37@users.noreply.github.com>
2025-12-24 13:37:58 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Merry Christmas';
+const titleCard = 'Tặng Hoa iu <3';
+const contentCard = 'Chúc em iu của anh Giáng sinh vui vẻ, anh yêu em nhiều lắm. Cảm ơn em vì chặng đường 1 năm qua <3';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
